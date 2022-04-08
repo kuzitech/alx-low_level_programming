337543d1423a000305b7beec39ad2473d05932e0
bhayor <bhayorbhayo@gmail.com>
2022-04-07 20:05:15 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 	char gram[] = "with proper grammar, but the outcome is a piece of art,";
-	printf("%s\n", gram);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
