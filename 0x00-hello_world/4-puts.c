249f847e21b73afb9a2c6c08fc400eb4f4fc4ab6
bhayor <bhayorbhayo@gmail.com>
2022-04-07 19:32:13 -0700

    fix puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
  **/
 int main(void)
 {
-	char puzzle [] = "Programming is like building a multilingual puzzle";
+	char puzzle[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(puzzle);
 	return (0);
