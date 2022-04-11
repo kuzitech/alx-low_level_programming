b97470f2c370fea5c54ef4a88722bdd6a4b67b64
bhayor <bhayorbhayo@gmail.com>
2022-04-11 00:00:31 -0700

    challenge solved

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,17 +15,19 @@ int main(void)
 	{
 		for (par2 = '0'; par2 <= '9'; par2++)
 		{
-			for (pre = '0'; pre <= '9'; pre++)
+			for (pre = par; pre <= '9'; pre++)
 			{
-				for (num = (pre + 1); num <= '9'; num++)
-				{
+				for (num = par2 + 1 ; num <= '9'; num++)
+				{		
 					putchar(par);
 					putchar(par2);
 					putchar(' ');
 					putchar(pre);
 					putchar(num);
-					if (!((par == '9' && par2 == '8') && (pre == '9' || num == '9')))
+					
+					if (!((par == '9' && par2 == '8') && (pre == '9' && num == '9')))
 					{
+						/**stop adding space and comma at 98 99**/
 						putchar(',');
 						putchar(' ');
 					}
