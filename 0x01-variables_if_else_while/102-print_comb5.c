4b71edea3b7177643ef6888e6730231589c9ac1a
bhayor <bhayorbhayo@gmail.com>
2022-04-12 15:59:29 -0700

    retry combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,11 +18,12 @@ int main(void)
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 
-			if (num1 == 98 && num2 == 99)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+			if (!(num1 == 98 && num2 == 99))
+			{
+				/*add the trailing comma and space*/
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 
