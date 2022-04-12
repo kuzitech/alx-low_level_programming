dcd3d92275bfbe724bc762f0c4b98ab31388ccb2
Bhayor <101385435+Bhayor@users.noreply.github.com>
2022-04-12 23:49:31 +0100

    redo combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,36 +6,27 @@
  */
 int main(void)
 {
-	int par;
-	int par2;
-	int pre;
-	int num;
+	int num1, num2;
 
-	for (par = '0'; par <= '9'; par++)
-	{
-		for (par2 = '0'; par2 <= '9'; par2++)
-		{
-			for (pre = par; pre <= '9'; pre++)
-			{
-				for (num = par2 + 1 ; num <= '9'; num++)
-				{
-					putchar(par);
-					putchar(par2);
-					putchar(' ');
-					putchar(pre);
-					putchar(num);
-					if (!((par == '9' && par2 == '8') && (pre == '9' && num == '9')))
-					{
-						/**stop adding space and comma at 98 99**/
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
+	for (num1 = 0; num1 <= 98; num1++)
+	{  /**first loop for the first two combinations**/
+		for (num2 = num1 + 1; num2 <= 99; num2++)
+		{ /**second loop for the last two combinations ending at 99**/
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar('\n');
 
+	putchar('\n');
 
 	return (0);
 }
