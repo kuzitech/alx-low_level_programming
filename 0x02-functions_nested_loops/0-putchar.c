81a0fe5de039384e76281fe77c7cad82da7eb200
bhayor <bhayorbhayo@gmail.com>
2022-04-12 19:33:24 -0700

    first putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * this program prints _putchar
+ * followed by a new line.
+ *
+ * Return: 0 (Success)
+ **/
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
