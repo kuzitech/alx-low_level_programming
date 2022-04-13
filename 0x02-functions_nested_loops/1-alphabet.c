29d4b2d3a31f0789a9c7813b288b0a59a0c16412
bhayor <bhayorbhayo@gmail.com>
2022-04-12 20:14:03 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,14 @@
  *
  * Return: 0 (success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char alpha;
 
 	for (alpha = 'a'; alpha <= 'z'; alpha++)
 	{
 		_putchar(alpha);
+
 		_putchar('\n');
 	}
 }
