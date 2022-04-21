c0150e7249996320e4d4c11f696caf2e7f7e5dce
bhayor <bhayorbhayo@gmail.com>
2022-04-20 21:22:47 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,12 @@
 
 void print_rev(char *s)
 {
-	int len = 0, i;
+	int i = 0;
 
-	while (s[i++])
-		len++;
+	while (s[i])
+		i++;
 
-	for (i = len - 1; i >= 0; i--)
+	while (i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
