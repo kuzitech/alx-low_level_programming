d43fac089e36c5873dd74302eeb2ebc5c9862d6b
Bhayor <bhayorbhayo@gmail.com>
2022-06-17 01:33:43 +0000

    bits in gap

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: number to print in binary
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, count = 0;
+	unsigned long int holder;
+
+	for (i = 63; i >= 0; i--)
+	{
+		holder = n >> i;
+
+		if (holder & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
